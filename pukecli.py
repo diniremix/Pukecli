0fedfef126c3d1c6445bebecf6e9182706694f35
Peter Anderson <diniremix@gmail.com>
2012-08-03 12:25:20 -0500

    agregada la funcion de limpiar segun el SO y la funcion de extraer

--- pukecli.py
+++ pukecli.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python
-import argparse
-import os
-os.system('cls')
+# -*- coding: UTF-8 -*-
+import argparse, sys, os
+
+if sys.platform.startswith('linux'):
+	os.system('clear')
+	print "la plataforma es linux"
+elif sys.platform.startswith('win'): 
+	os.system('cls')
+	print "la plataforma es windows"
+
+def extractpuke(fich):
+	"Extrae un archivo puke"
+	pass
 
 parser = argparse.ArgumentParser(usage='%(prog)s [opciones]')
 parser.add_argument("-e", help="Extrae un fichero .puke",type=extractpuke)
-parser.add_argument("-c", help="Crea un fichero .puke",type=createpuke)
 parser.add_argument("-v", help="Muestra la version actual de pukecli", 
 					action='version',version='%(prog)s 0.2')
 args = parser.parse_args()
