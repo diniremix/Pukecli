1e15bd1e0dc191838e9c3a99343e21f42922d6b3
Peter Anderson <diniremix@gmail.com>
2012-08-03 12:31:15 -0500

    agregada la funcion de extraer el fichero puke el encoding es utf8

--- pukecli.py
+++ pukecli.py
@@ -11,10 +11,33 @@ elif sys.platform.startswith('win'):
 
 def extractpuke(fich):
 	"Extrae un archivo puke"
+	if os.path.isfile(fich):
+		try:
+			import zipfile
+			if zipfile.is_zipfile(fich):
+				print "Procesando archivo puke..."
+				zip=zipfile.ZipFile(fich,"r")
+				print "Listando informacion del archivo"
+				print 
+				print zip.printdir()
+				print 
+				print "Extrayendo el archivo",fich, "a puke/"
+				zip.extractall("puke")
+				zip.close()
+			else:
+				print "El archivo",fich,"no es un archivo puke valido o esta dañado"
+		except:
+			print "Ocurrio un error al procesar el archivo",fich
+	else:
+		print fich,"El archivo no existe en la ruta especificada"
+
+def listpuke(fich):
+	"Listar el contenido de un archivo puke"
 	pass
 
 parser = argparse.ArgumentParser(usage='%(prog)s [opciones]')
 parser.add_argument("-e", help="Extrae un fichero .puke",type=extractpuke)
+parser.add_argument("-l", help="Lista un fichero .puke",type=listpuke)
 parser.add_argument("-v", help="Muestra la version actual de pukecli", 
 					action='version',version='%(prog)s 0.2')
 args = parser.parse_args()
